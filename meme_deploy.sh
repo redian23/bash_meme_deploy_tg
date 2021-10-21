9046995bb9a71a56b88b5932669a901a87829a83
Redian23 <vasya.degski@gmail.com>
2021-10-22 00:00:33 +0300

    Code Refactoring; Fix multi_deploy; add filter for config's

--- meme_deploy.sh
+++ meme_deploy.sh
@@ -3,7 +3,7 @@ work_path=$(echo -e `pwd`)
 date=$(date '+%d/%m/%Y-%H:%M:%S');
 
 source /etc/meme-deploy/*
-config_files=($(ls /etc/meme-deploy/ | awk {'print $1'}))
+config_files=($(ls /etc/meme-deploy/*.conf | awk {'print $1'}))
 
 log_file=/var/log/meme-deploy.log
 
@@ -23,23 +23,26 @@ deploy(){
     source $1
     for meme_num in ${memes_list[@]}
     do
-        curl https://api.telegram.org/bot${BotToken}/sendphoto -F "chat_id=$CHAT_ID" -F "photo=@$meme_folder/$meme_num";
-        echo "${date} Meme ${meme_num} Succsess send to TG Channal" | tee -a $log_file
+        curl -s https://api.telegram.org/bot${BotToken}/sendphoto -F "chat_id=$CHAT_ID" -F "photo=@$meme_folder/$meme_num" > /dev/null && \
+        echo "${date} Meme ${meme_num} Succsess send to ${channal_name}" | tee -a $log_file
+        break
+    done
+}
 
-        mv $meme_folder/$meme_num $meme_folder/posted/ & \
+move_to_posted(){
+        mv $meme_folder/$meme_num $meme_folder/posted/ && \
         echo "${date} Meme ${meme_num} moved on 'posted' folder" | tee -a $log_file
 
         local wait_time=$(shuf -i 45-60 -n 1)
-        echo "${date} Next Meme ${meme_num} will be post in ${wait_time} minute" | tee -a $log_file
-        sleep ${wait_time}m
-    done
+        echo "${date} Next Meme will be post in ${wait_time} minute" | tee -a $log_file
 }
 
 multi_deploy(){
     for stream in "${config_files[@]}"
     do
-       deploy $stream | xargs -P 4 -n 1
+       deploy $stream
     done
+        move_to_posted
 }
 
 echo "Meme Deploy Run ^-^"
@@ -47,4 +50,4 @@ scan_meme_folder
 multi_deploy
 
 #help
-#https://unix.stackexchange.com/questions/47695/how-to-write-startup-script-for-systemd  
+#https://unix.stackexchange.com/questions/47695/how-to-write-startup-script-for-systemd
