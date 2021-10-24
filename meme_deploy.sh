8f7b5e9dbe2a751d625b442556a1de430bdadd7e
vasiliy-klimov-idf <vasiliy.klimov@moneyman.ru>
2021-10-24 07:19:52 +0300

    Fix multi senting

--- meme_deploy.sh
+++ meme_deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 work_path=$(echo -e `pwd`)
-date=$(date '+%d/%m/%Y-%H:%M:%S');
 
 source /etc/meme-deploy/*
 config_files=($(ls /etc/meme-deploy/*.conf | awk {'print $1'}))
@@ -19,32 +18,47 @@ scan_meme_folder(){
     fi
 }
 
-deploy(){
+sent_to_tg(){
+    local date=$(date '+%d/%m/%Y-%H:%M:%S')
+
     source $1
     for meme_num in ${memes_list[@]}
     do
         curl -s https://api.telegram.org/bot${BotToken}/sendphoto -F "chat_id=$CHAT_ID" -F "photo=@$meme_folder/$meme_num" > /dev/null && \
         echo "${date} Meme ${meme_num} Succsess send to ${channal_name}" | tee -a $log_file
-
-
-        if [ $1 == "${config_files[-1]}" ];then
-                mv $meme_folder/$meme_num $meme_folder/posted/ && \
-                echo "${date} Meme ${meme_num} moved on 'posted' folder" | tee -a $log_file
-        fi
-
-        local wait_time=$(shuf -i 45-60 -n 1)
-        echo "${date} Next Meme will be post in ${wait_time} minute" | tee -a $log_file
-        sleep ${wait_time}m
+        break
     done
 }
 
-multi_deploy(){
+move_to_posted(){
+    local date=$(date '+%d/%m/%Y-%H:%M:%S')
+
+    if [ $stream == "${config_files[-1]}" ];then
+        mv $meme_folder/$meme_num $meme_folder/posted/ && \
+        echo "${date} Meme ${meme_num} moved on 'posted' folder" | tee -a $log_file
+    fi
+}
+
+run_scheduler(){
+    local date=$(date '+%d/%m/%Y-%H:%M:%S')
+    local wait_time=$(shuf -i 45-60 -n 1)
+
+    echo "${date} Next Meme will be post in ${wait_time} minute" | tee -a $log_file
+    sleep ${wait_time}m
+}
+
+multi_senting(){
     for stream in "${config_files[@]}"
     do
-        deploy $stream
+        sent_to_tg $stream
     done
 }
 
 echo "Meme Deploy Run ^-^"
-scan_meme_folder
-multi_deploy
+while :
+do
+    scan_meme_folder
+    multi_senting
+    move_to_posted
+    run_scheduler
+done
